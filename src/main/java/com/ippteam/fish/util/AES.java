0aa0f264f4d67329696a2e713567c3b4f7c47fb4
isunimp <ansheck@163.com>
2016-11-01 20:43:13 +0800

    添加了一些工具类

---
+++ src/main/java/com/ippteam/fish/util/AES.java
@@ -0,0 +1,108 @@
+package com.ippteam.fish.util;
+
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.UUID;
+
+/**
+ * Created by pactera on 16/10/31.
+ */
+public class AES {
+
+    public static String newSecretKey() {
+        String uuidString = UUID.randomUUID().toString();
+        int len = uuidString.length();
+        int i = -1;
+        int bi = -1;
+        char[] buff = new char[len];
+
+        while (++i < len) {
+            char c = uuidString.charAt(i);
+            if (c != '-') {
+                buff[++bi] = c;
+            }
+        }
+        return new String(buff, 0, bi + 1);
+    }
+
+    /**
+     * 加密
+     *
+     * @param content     需要加密的内容
+     * @param securityKey 加密密码
+     * @return
+     */
+    public static byte[] encrypt(String content, String securityKey) {
+        if (null == securityKey || securityKey.length() == 0) {
+            throw new NullPointerException("key not is null");
+        }
+        try {
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            random.setSeed(securityKey.getBytes());
+            KeyGenerator kgen = KeyGenerator.getInstance("AES");
+            kgen.init(128, random);
+            SecretKey secretKey = kgen.generateKey();
+            byte[] enCodeFormat = secretKey.getEncoded();
+            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
+            byte[] byteContent = content.getBytes("utf-8");
+            cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
+            byte[] result = cipher.doFinal(byteContent);
+            return result; // 加密
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 解密
+     *
+     * @param content     待解密内容
+     * @param securityKey 解密密钥
+     * @return
+     */
+    public static byte[] decrypt(byte[] content, String securityKey) {
+        if (null == securityKey || securityKey.length() == 0) {
+            throw new NullPointerException("key not is null");
+        }
+        try {
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            random.setSeed(securityKey.getBytes());
+            KeyGenerator kgen = KeyGenerator.getInstance("AES");
+            kgen.init(128, random);
+            SecretKey secretKey = kgen.generateKey();
+            byte[] enCodeFormat = secretKey.getEncoded();
+            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
+            cipher.init(Cipher.DECRYPT_MODE, key);// 初始化
+            byte[] result = cipher.doFinal(content);
+            return result; // 加密
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/ippteam/fish/util/Date.java
@@ -0,0 +1,12 @@
+package com.ippteam.fish.util;
+
+/**
+ * Created by pactera on 16/10/28.
+ */
+public class Date {
+
+    public static long getTime(){
+        return System.currentTimeMillis();
+    }
+
+}
