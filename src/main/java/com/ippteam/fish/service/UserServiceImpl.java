da62a9c19f447c5adaf35b1cc20c82168aa07503
isunimp <ansheck@163.com>
2016-11-02 21:28:03 +0800

    根据mybatis generator生成的dao/entity/mapper重新实现Service

--- src/main/java/com/ippteam/fish/service/UserServiceImpl.java
+++ src/main/java/com/ippteam/fish/service/UserServiceImpl.java
@@ -1,32 +1,42 @@
 package com.ippteam.fish.service;
 
-import com.ippteam.fish.dao.IUserDao;
+import com.ippteam.fish.dao.UserMapper;
 import com.ippteam.fish.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 /**
  * Created by pactera on 16/10/24.
  */
 
-@Service("UserServiceImpl")
-public class UserServiceImpl implements IUserService {
+@Service("UserService")
+public class UserServiceImpl implements UserService {
 
     @Autowired
-    private IUserDao userDao;
+    private UserMapper userDao;
 
     public User getUserById(Integer id) {
-        return userDao.getUserByID(id);
+        return userDao.selectByPrimaryKey(id);
     }
 
 
     public Boolean login(String userName, String pwd) {
-        User user = userDao.getUserByUserName(userName);
-        if (user != null && user.getPassword().equals(pwd)) {
-            return true;
-        } else {
-            return false;
+        UserExample userExample = new UserExample();
+        userExample.or().andUserNameEqualTo(userName);
+
+        List<User> users = userDao.selectByExample(userExample);
+        if (users.size() > 0) {
+            User user = users.get(0);
+            if (user != null && user.getPassword().equals(pwd)) {
+                return true;
+            } else {
+                return false;
+            }
         }
+        return false;
     }
 
     public Boolean register(User user) {
