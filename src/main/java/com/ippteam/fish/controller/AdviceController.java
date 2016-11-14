280e3dd4ff6bbdb389665b64ec8c49f5d07c2d81
isunimp <ansheck@163.com>
2016-11-14 18:16:03 +0800

    新增error级别日志文件记录

--- src/main/java/com/ippteam/fish/controller/AdviceController.java
+++ src/main/java/com/ippteam/fish/controller/AdviceController.java
@@ -1,13 +1,19 @@
 package com.ippteam.fish.controller;
 
 import com.ippteam.fish.util.api.entity.*;
-import com.ippteam.fish.util.api.exception.InvalidRequest;
+import com.ippteam.fish.util.api.exception.*;
+import com.ippteam.fish.util.api.interceptor.CertificaInterceptor;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import sun.management.snmp.jvmmib.JvmThreadInstanceTableMeta;
+
+import static com.ippteam.fish.util.Final.*;
 
 /**
  * Created by pactera on 16/10/26.
@@ -16,24 +22,59 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class AdviceController {
 
-    @ExceptionHandler(InvalidRequest.class)
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    private static Logger logger = Logger.getLogger(AdviceController.class);
+
+    @ExceptionHandler(BusinessException.class)
+    @ResponseStatus()
     @ResponseBody
-    public Result InvalidRequestHandler(Exception e) {
+    public Result businessException(BusinessException e) {
+        return new Result(e.getCode(), e.getReason(), null);
+    }
+
+    @ExceptionHandler(CertificationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ResponseBody
+    public Result certificationException(CertificationException e) {
+        return new Result(401, e.getMessage(), null);
+    }
+
+    @ExceptionHandler(ParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public Result parameterException(ParameterException e) {
         return new Result(400, e.getMessage(), null);
     }
 
-    @ExceptionHandler
-    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseBody
-    public Result exceptionHandler(Exception e) {
-        return new Result(500, e.getClass().getName() + "." + e.getMessage(), null);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Result httpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return new Result(400, EXCEPTION_REQUEST_BODY_PARAM_MISSING, null);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    @ResponseStatus(value = HttpStatus.METHOD_NOT_ALLOWED)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     @ResponseBody
-    public Result NotSupportedHandler(Exception e) {
-        return new Result(405, "Request method not supported or Request not found", null);
+    public Result notSupportedHandler(Exception e) {
+        return new Result(405, EXCEPTION_REQUEST_METHOD_NOTSUPPORTED, null);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public Result exceptionHandler(Exception e) {
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("==================================================\n");
+        sb.append(e.getClass().getName() + "\n");
+        sb.append(e.getMessage() + "\n");
+
+        for (StackTraceElement item : e.getStackTrace()) {
+            String className = item.getClassName();
+            int lineNumber = item.getLineNumber();
+            sb.append("at\t" + className + "(" + lineNumber + ")" + "\n");
+        }
+        logger.error(sb.toString());
+        return new Result(500, EXCEPTION_UNKNOWN_EXCEPTION, null);
     }
 }
