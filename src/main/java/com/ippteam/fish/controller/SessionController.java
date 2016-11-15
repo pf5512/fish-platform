fbc02c6002384d9b88bf51cb3c265a1b4065dad5
isunimp <ansheck@163.com>
2016-11-15 13:19:32 +0800

    登陆实现

--- src/main/java/com/ippteam/fish/controller/SessionController.java
+++ src/main/java/com/ippteam/fish/controller/SessionController.java
@@ -1,13 +1,24 @@
 package com.ippteam.fish.controller;
 
 import com.ippteam.fish.entity.User;
+import com.ippteam.fish.model.Login;
+import com.ippteam.fish.util.Session;
+import com.ippteam.fish.util.Verify;
+import com.ippteam.fish.util.api.BusinessStatus;
+import com.ippteam.fish.util.api.entity.Credential;
 import com.ippteam.fish.util.api.entity.Result;
+import com.ippteam.fish.util.api.exception.*;
 import com.ippteam.fish.util.api.version.ApiVersion;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import static com.ippteam.fish.util.Final.*;
 
 /**
  * Created by isunimp on 16/11/2.
@@ -16,15 +27,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/api/{version}/session")
 public class SessionController extends BaseController {
-
     @ApiVersion(1)
     @ResponseBody
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public Result login(@RequestBody(required = false) User user) {
-        if (userService.login(user.getUserName(), user.getPassword())) {
-            return new Result(200, null, true);
-        } else {
-            return new Result(200, "账号或密码错误.", false);
+    public Result login(@RequestBody Login login, HttpServletRequest request) {
+        if (!Verify.string(login.getAccount()) ||
+                !Verify.string(login.getPassword())) {
+            throw new ParameterException(EXCEPTION_REQUEST_BODY_PARAM_INVALID);
         }
+
+        User user = userService.login(login.getAccount(), login.getPassword());
+        if (user == null) {
+            throw new BusinessException(BusinessStatus.USERNAME_OR_PASSWORD_INVALID);
+        }
+        Credential credential = Session.login(request, user);
+        return new Result(0, null, credential.getToken());
     }
 }
