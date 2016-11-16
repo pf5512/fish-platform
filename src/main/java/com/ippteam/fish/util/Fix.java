667eefaf6aac7746ab58aec7e266f0d9289e9869
isunimp <ansheck@163.com>
2016-11-16 10:03:54 +0800

    新增安全取List

---
+++ src/main/java/com/ippteam/fish/util/Fix.java
@@ -0,0 +1,16 @@
+package com.ippteam.fish.util;
+
+import org.jetbrains.annotations.Contract;
+
+import java.util.List;
+
+/**
+ * Created by isunimp on 16/11/16.
+ */
+public class Fix {
+
+    static public <E> E list(List<E> list, int index) {
+        return list != null && list.size() > 0 && index > 0 && list.size() > index ? list.get(index) : null;
+    }
+
+}
